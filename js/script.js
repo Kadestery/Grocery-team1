38bf469af232206e9305e901551704daa8fc565a
Gabriel Dubois <gabrieldubois@Gabriels-MacBook-Pro.local>
2022-02-04 11:39:46 -0500

    home

--- js/script.js
+++ js/script.js
@@ -4,23 +4,39 @@ let easter = document.querySelector('.easter a');
 document.querySelector('#search-btn').onclick = () => {
     searchForm.classList.toggle('active');
     easter.classList.toggle('active');
+
+    cart.classList.remove('active');
+    navbar.classList.remove('active');
+    login.classList.remove('active');
 }
 
 let cart = document.querySelector('.cart');
 
 document.querySelector('#cart-btn').onclick = () => {
     cart.classList.toggle('active');
+
+    searchForm.classList.remove('active');
+    navbar.classList.remove('active');
+    login.classList.remove('active');
 }
 
 let login = document.querySelector('.login-form');
 
 document.querySelector('#log-btn').onclick = () => {
     login.classList.toggle('active');
+
+    searchForm.classList.remove('active');
+    cart.classList.remove('active');
+    navbar.classList.remove('active');
 }
 
 let navbar = document.querySelector('.navbar');
 
 document.querySelector('#menu-btn').onclick = () => {
     navbar.classList.toggle('active');
+
+    searchForm.classList.remove('active');
+    cart.classList.remove('active');
+    login.classList.remove('active');
 }
 // 
