d062466780a216a27ddcdf810251a7fb906f1339
Xavier Guertin <xavierguertin@MacBook-Pro-de-Xavier.local>
2022-03-31 14:07:30 -0400

    Cart Price

--- js/script.js
+++ js/script.js
@@ -81,3 +81,78 @@ function updatePrice() {
     return document.querySelector(".product-price").innerHTML = '$' + total + ' for ' + quantity + ' ' + type + '(s)';
 }
 
+// Cart
+
+function updateCartPrice() {
+    var quantity = document.getElementById("quantity").value;
+    var unitPrice = parseFloat(document.querySelector("#unitPrice").innerHTML);
+    total = (quantity * unitPrice).toFixed(2);
+    document.querySelector("#product-price").innerHTML = total;
+    update();
+}
+function updateCartPrice2() {
+    var quantity = document.getElementById("quantity2").value;
+    var unitPrice = parseFloat(document.querySelector("#unitPrice2").innerHTML);
+    total = (quantity * unitPrice).toFixed(2);
+    document.querySelector("#product-price2").innerHTML = total;
+    update();
+}
+function updateCartPrice3() {
+    var quantity = document.getElementById("quantity3").value;
+    var unitPrice = parseFloat(document.querySelector("#unitPrice3").innerHTML);
+    total = (quantity * unitPrice).toFixed(2);
+    document.querySelector("#product-price3").innerHTML = total;
+    update();
+    
+}
+//function that start the main functions
+function update()
+{
+    updateQuantities();
+    updateSubTotalCart();
+    updateGST();
+    updateQST();
+    updateTotalPriceCart();
+}
+//Total price Cart
+function updateSubTotalCart()
+{
+    var tot = parseFloat(document.querySelector("#product-price").innerHTML);
+    var tot2 = parseFloat(document.querySelector("#product-price2").innerHTML);
+    var tot3 = parseFloat(document.querySelector("#product-price3").innerHTML);
+    total = (tot + tot2 + tot3).toFixed(2);
+    document.querySelector(".subTotal").innerHTML = total;
+}
+//Update quantities
+function updateQuantities()
+{
+    var quant = parseFloat(document.getElementById("quantity").value);
+    var quant2 = parseFloat(document.getElementById("quantity2").value);
+    var quant3 = parseFloat(document.getElementById("quantity3").value);
+    total = (quant + quant2 + quant3);
+    document.querySelector(".nbItems").innerHTML = total;
+}
+function updateGST()
+{
+    var tot = parseFloat(document.querySelector("#product-price").innerHTML);
+    var tot2 = parseFloat(document.querySelector("#product-price2").innerHTML);
+    var tot3 = parseFloat(document.querySelector("#product-price3").innerHTML);
+    total = (0.05*(tot + tot2 + tot3)).toFixed(2);
+    document.querySelector(".GST").innerHTML = total;
+}
+function updateQST()
+{
+    var tot = parseFloat(document.querySelector("#product-price").innerHTML);
+    var tot2 = parseFloat(document.querySelector("#product-price2").innerHTML);
+    var tot3 = parseFloat(document.querySelector("#product-price3").innerHTML);
+    total = (0.10*(tot + tot2 + tot3)).toFixed(2);
+    document.querySelector(".QST").innerHTML = total;
+}
+function updateTotalPriceCart()
+{
+    var tot = parseFloat(document.querySelector("#product-price").innerHTML);
+    var tot2 = parseFloat(document.querySelector("#product-price2").innerHTML);
+    var tot3 = parseFloat(document.querySelector("#product-price3").innerHTML);
+    total = (1.15*(tot + tot2 + tot3)).toFixed(2);
+    document.querySelector(".totalPrice").innerHTML = total;
+}
