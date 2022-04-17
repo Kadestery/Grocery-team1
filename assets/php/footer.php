55d6995d9c594ec72b767813933ef55c183a1e3c
TheOneWithTheShot <xavierguertin@gmail.com>
2022-04-17 17:11:32 -0400

    Update

--- assets/php/footer.php
+++ assets/php/footer.php
@@ -7,50 +7,50 @@
                     <a target="_blank" href="https://www.linkedin.com/in/ali-fetanat-42216521b/">Ali Fetanat</a>
                 </li>
                 <li>
-                    <a target="_blank" href="">Elliot Vinet</a>
+                    <a target="_blank" href="./underConstruction.php">Elliot Vinet</a>
                 </li>
                 <li>
                     <a target="_blank" href="https://www.linkedin.com/in/gabriel-dubois-soen/">Gabriel Dubois</a>
                 </li>
                 <li>
-                    <a target="_blank" href="">Kade Keating</a>
+                    <a target="_blank" href="./underConstruction.php">Kade Keating</a>
                 </li>
                 <li>
-                    <a target="_blank" href="">Minh Thuan Huynh</a>
+                    <a target="_blank" href="./underConstruction.php">Minh Thuan Huynh</a>
                 </li>
                 <li>
                     <a target="_blank" href="https://ca.linkedin.com/in/xavier-guertin-b896a71a6">Xavier Guertin</a>
                 </li>
             </ul>
         </div>
-        <div class="jobs">
+        <div class="team">
             <h1>Jobs</h1>
             <ul>
                 <li>
-                    <p>Jobs in stores</p>
+                    <a target="_blank" href="./underConstruction.php"> Jobs in stores</a>
                 </li>
                 <li>
-                    <p>Careers at head office</p>
+                    <a target="_blank" href="./underConstruction.php">Careers at head office</a>
                 </li>
             </ul>
         </div>
-        <div class="customer">
+        <div class="team">
             <h1>Customer service</h1>
             <ul>
                 <li>
-                    <p>Contact us</p>
+                    <a target="_blank" href="./underConstruction.php">Contact Us</a>
                 </li>
                 <li>
-                    <p>Terms and conditions</p>
+                    <a target="_blank" href="./underConstruction.php">Terms And Conditions</a>
                 </li>
                 <li>
-                    <p>Privacy policy</p>
+                    <a target="_blank" href="./underConstruction.php">Privacy Policy</a>
                 </li>
                 <li>
-                    <p>Find a store</p>
+                    <a target="_blank" href="https://www.google.com/maps/place/Challenger+Deep/@11.3733328,142.591667,14z/data=!3m1!4b1!4m5!3m4!1s0x67107c6bf58b799f:0xce8948eec23ac2bb!8m2!3d11.373333!4d142.591667">Find a Store</a>
                 </li>
                 <li>
-                    <p>FAQ</p>
+                    <a target="_blank" href="./underConstruction.php">FAQ</a>
                 </li>
             </ul>
         </div>
--- assets/php/header.php
+++ assets/php/header.php
@@ -1,6 +1,35 @@
 <?php session_start(); ?>
-<header class="header">
-        <a href="./index.php" class="logo"> <i class="bx bxs-cricket-ball"></i> 50/10</a>
+<!DocTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aisles page</title>
+
+    <!-- boxicon link -->
+    <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet'>
+
+    <!-- import css -->
+    <link rel="stylesheet" href="./css/styles.css">
+
+    <!-- favicon (title of page image) -->
+    <link rel="icon" href="/images/favicon.JPG" sizes="32x32" type="image/JPG">
+
+    <script src="https://code.jquery.com/jquery-3.3.1.js" integity="sha256-2Kok7Mb0yxpgUVvAk/H32jig0SYS2auk4Pfzbm7uH60=" crossorigin "anonymous"></script>
+    <script>
+        $(function() {
+            $('#footer').load("./assets/php/footer.php");
+        });
+    </script>
+
+</head>
+
+<body>
+    <!-- Header -->
+    <header class="header">
+        <a href="./index.php " class="logo"> <i class="bx bxs-cricket-ball"></i> 50/10</a>
         <!-- menu -->
         <nav class="navbar">
             <a href="./index.php">home</a>
@@ -20,10 +49,51 @@
         </div>
         <!-- buttons -->
         <div class="icons">
-            <a class='bx bx-menu-alt-right' id="menu-btn"></a>
-            <!--a class='bx bx-search-alt' id="search-btn"></a-->
-            <a href= "./p4_cart.php" class='bx bx-cart'></a>
-            <a href="./p5_login.php" class='bx bx-log-in-circle' id="log-btn"></a>
+            <div class='bx bx-menu-alt-right' id="menu-btn"></div>
+            <div class='bx bx-search-alt' id="search-btn"></div>
+            <div class='bx bx-cart' id="cart-btn"></div>
+            <div class='bx bx-log-in-circle' id="log-btn"></div>
         </div>
-        
-</header>
+        <!-- search -->
+        <form action="" class="search-form">
+            <input type="search" id="search-box" placeholder="Search...">
+            <label for="search-box" class="bx bx-search-alt"></label>
+        </form>
+        <!-- cart -->
+        <div class="cart">
+            <div class="box">
+                <i class='bx bx-trash'></i>
+                <img src="./images/k.jpg" alt="">
+                <div class="content">
+                    <h3>Name</h3>
+                    <span class="price">price$</span>
+                    <span class="quantity">qty: variable</span>
+                </div>
+            </div>
+            <div class="box">
+                <i class='bx bx-trash'></i>
+                <img src="./images/k.jpg" alt="">
+                <div class="content">
+                    <h3>name</h3>
+                    <span class="price">price$</span>
+                    <span class="quantity">qty: variable</span>
+                </div>
+            </div>
+            <div class="total">total: totalPrice$</div>
+            <a href="./p4_cart.php" class="btn">Review Items</a>
+        </div>
+
+        <!-- login -->
+
+        <form action="" class="login-form">
+            <h3>log in</h3>
+            <input type="email" placeholder="enter email" class="box">
+            <input type="password" placeholder="enter password" class="box">
+            <br>
+            <a href="https://alzheimer.ca/sites/default/files/documents/alzheimers-disease_getting-a-diagnosis_0.pdf" target="_blank">forgot your password?</a>
+
+            <a href="./p6_signup.php">Create An Account</a>
+            <a href="#" class="btn"> Login Now </a>
+        </form>
+    </header>
+    <!--END OF HEADER-->
