cace00986bd1ff1c590c2175362416e4f6860bf5
TheOneWithTheShot <xavierguertin@gmail.com>
2022-04-17 16:25:54 -0400

    Updated

--- p3_IceCream.php
+++ p3_IceCream.php
@@ -22,81 +22,15 @@
         $(function() {
             $('#footer').load("./assets/php/footer.php");
         });
+        $(function() {
+            $('#header').load("./assets/php/header.php");
+        });
     </script>
-
 </head>
-
 
 <body>
     <!-- Header -->
-    <header class="header">
-        <a href="./index.php " class="logo"> <i class="bx bxs-cricket-ball"></i> 50/10</a>
-        <!-- menu -->
-        <nav class="navbar">
-            <a href="./index.php">home</a>
-            <a href="#aisles" id="aisles-btn">Aisles</a>
-            <a href="./about.php">about us</a>
-            <a href="./p7_backstore.php">Backstore</a>
-        </nav>
-        <div class="aisles-menu">
-            <a href="./p2_fruitaisle.php">Fruits</a>
-            <a href="./p2_VegetablesAisle.php">Vegetables</a>
-            <a href="./p2_DairyAisle.php">Dairy</a>
-            <a href="./p2_MeatAisle.php">Meat</a>
-            <a href="./p2_PastryAisle.php">Pastry</a>
-            <a href="./p2_SnacksAisle.php">Snacks</a>
-            <a href="./p2_SeafoodAisle.php">Seafood</a>
-            <a href="./p2_WaterAisle.php">Drinks</a>
-        </div>
-        <!-- buttons -->
-        <div class="icons">
-            <div class='bx bx-menu-alt-right' id="menu-btn"></div>
-            <div class='bx bx-search-alt' id="search-btn"></div>
-            <div class='bx bx-cart' id="cart-btn"></div>
-            <div class='bx bx-log-in-circle' id="log-btn"></div>
-        </div>
-        <!-- search -->
-        <form action="" class="search-form">
-            <input type="search" id="search-box" placeholder="Search...">
-            <label for="search-box" class="bx bx-search-alt"></label>
-        </form>
-        <!-- cart -->
-        <div class="cart">
-            <div class="box">
-                <i class='bx bx-trash'></i>
-                <img src="./images/k.jpg" alt="">
-                <div class="content">
-                    <h3>Name</h3>
-                    <span class="price">price$</span>
-                    <span class="quantity">qty: variable</span>
-                </div>
-            </div>
-            <div class="box">
-                <i class='bx bx-trash'></i>
-                <img src="./images/k.jpg" alt="">
-                <div class="content">
-                    <h3>name</h3>
-                    <span class="price">price$</span>
-                    <span class="quantity">qty: variable</span>
-                </div>
-            </div>
-            <div class="total">total: totalPrice$</div>
-            <a href="./p4_cart.php" class="btn">Review Items</a>
-        </div>
-
-        <!-- login -->
-
-        <form action="" class="login-form">
-            <h3>log in</h3>
-            <input type="email" placeholder="enter email" class="box">
-            <input type="password" placeholder="enter password" class="box">
-            <br>
-            <a href="https://alzheimer.ca/sites/default/files/documents/alzheimers-disease_getting-a-diagnosis_0.pdf" target="_blank">forgot your password?</a>
-
-            <a href="./p6_signup.php">Create An Account</a>
-            <a href="#" class="btn"> Login Now </a>
-        </form>
-    </header>
+    <div id="header"></div>
     <!--END OF HEADER-->
 
 
